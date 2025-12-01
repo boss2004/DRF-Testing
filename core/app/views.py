b3783e12e3b792c88db0410dcb125693daa2d91c
Igor <igorgorbun0ff@yandex.ru>
2025-12-01 13:53:19 +0300

    Hello

--- core/app/views.py
+++ core/app/views.py
@@ -56,3 +56,4 @@ class PostDetailAPIView(APIView):
         post.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
     print("Нет нельзя тут создавать лишнее")
+    print("Привет из ветки feature/createuser")
